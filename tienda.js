a7951d4d8429fd7011e4a4d819ef87eb2d9a4fa8
Mery Agüero <aguero.f.marisol@gmail.com>
2022-10-19 23:04:29 -0300

    Cambios en tienda y página inicial

--- tienda.js
+++ tienda.js
@@ -1,3 +1,17 @@
+/*menú*/
+const cerrar = document.querySelector(".cerrar");
+const abrir = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+
+cerrar.addEventListener("click", () => {
+  menu.style.visibility = "esconder";
+});
+abrir.addEventListener("click", () => {
+  menu.style.visibility = "ver";
+});
+
+
+/*formulario de pago*/
 const productButton = document.querySelector(".productButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
@@ -9,4 +23,5 @@ productButton.addEventListener("click", () => {
 close.addEventListener("click", () => {
   payment.style.display = "none";
 });
+
 
